0db767f161604808f57938b9cc622799f1920348
Brian Bell <bbell.semm@gmail.com>
2023-09-04 11:25:06 -0400

    Created TaskList.js

---
+++ my-react-dashboard/src/components/TaskList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function taskList() {
+    return (
+        <div>
+            <h2>Task Tracker</h2>
+            {/* We'll add more here soon */}
+        </div>
+    );
+}
