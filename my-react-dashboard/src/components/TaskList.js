87589d58d4a9b6fb7046c6f57810a023a0effcce
Brian Bell <bbell.semm@gmail.com>
2023-09-04 11:40:43 -0400

    state management & form to create new tasks

--- my-react-dashboard/src/components/TaskList.js
+++ my-react-dashboard/src/components/TaskList.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import { useState } from 'react';
+
 function taskList() {
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState(''); //to track the input field
+
+    const addTask = () => {
+        if(newTask) {
+            setTasks([...tasks, newTask]);
+            setNewTask('');
+        }
+    };
+
+
     return (
         <div>
             <h2>Task Tracker</h2>
-            {/* We'll add more here soon */}
+            
+            <div>
+                <input
+                    type = "text"
+                    value={newTask.name}
+                    onChange={e => setNewTask(e.target.value)}
+                    placeholder="New task..." 
+                />
+                <button onClick={addTask}>Add</button>
+            </div>
+
+            {/* We'll display tasks here soon */}
         </div>
     );
 }
